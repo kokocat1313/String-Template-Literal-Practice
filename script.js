7e15a98a44e831d2a7e87d11aad671892ebe1a9c
kokocat1313 <66399733+kokocat1313@users.noreply.github.com>
2020-10-24 16:42:28 -0700

    1st commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const dice1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const dice2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+// YOUR CODE BELOW THIS LINE:
+let roll = `You rolled a ${dice1} and a ${dice2}. They sum to ${dice1 + dice2}`
+
+const dice1 = Math.floor(Math.random() * 6) + 1;
+const dice2 = Math.floor(Math.random() * 6) + 1;
+
+let roll = `You rolled a ${dice1} and a ${dice2}. They sum to ${dice1 + dice2}`
